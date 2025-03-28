7adbb975b73aae1bc2b8ff59f9c1a1ae6103a24b
Malakin1116 <malyakinabogdan@gmail.com>
2025-03-28 15:43:11 -0700

    back

--- src/server.js
+++ src/server.js
@@ -26,20 +26,17 @@ export const startServer = async () => {
     }),
   );
 
- app.use(
-  cors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'http://localhost:8081',
-      'https://bug1116.onrender.com',
-      true, // Дозволяємо запити без origin
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }),
-);
+app.use(
+    cors({
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:8081',
+        'https://nodejs-hw-mongodb-1-vszm.onrender.com',
+      ],
+      credentials: true,
+    }),
+  );
 
   app.use(cookieParser());
 
